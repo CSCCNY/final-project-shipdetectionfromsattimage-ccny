7dc6469af68296f8d71e78d9488b5320662f2ce7
SujoyDU <debnath.sujoy@hotmail.com>
2021-04-26 12:45:04 -0400

    added basic CNN

---
+++ model/BasicCNN.py
@@ -0,0 +1,15 @@
+'''
+-------------------------------
+Network Architecture
+1.Convolution, Filter shape:(5,5,6), Stride=1, Padding=’SAME’
+2. Max pooling (2x2), Window shape:(2,2), Stride=2, Padding=’Same’
+3. ReLU
+4. Convolution, Filter shape:(5,5,16), Stride=1, Padding=’SAME’
+5. Max pooling (2x2), Window shape:(2,2), Stride=2, Padding=’Same’
+6. ReLU
+7. Fully Connected Layer (128)
+8. ReLU
+9. Fully Connected Layer (10)
+10. Softmax
+------------------------------------------
+'''
