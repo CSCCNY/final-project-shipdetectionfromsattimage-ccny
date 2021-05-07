c1606601afb4d59eaef37e0c51b05ef402273412
SujoyDU <debnath.sujoy@hotmail.com>
2021-05-07 01:04:27 -0400

    added data generation

--- model/data/data_generate.py
+++ model/data/data_generate.py
@@ -24,6 +24,40 @@ AugmentedDir = "../generatedData/"
 BOX_COLOR = (255, 0, 0) # Red
 TEXT_COLOR = (255, 255, 255) # White
 
+def drawImage(imagePath,imageData,className):
+    # Input image
+    image = cv2.imread(imagePath)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (50, 50)
+    fontScale = 1
+    color = (255, 0, 0)
+    thickness = 20
+    cv2.rectangle(image, (int(imageData[0]), int(imageData[1])), (int(imageData[2]), int(imageData[3])), (0, 255, 0), 2)
+
+    # box =cv2.boxPoints(
+    #     ((imageData['bbox_angle'][0],imageData['bbox_angle'][1]),
+    #     (imageData['bbox_angle'][2],imageData['bbox_angle'][3]),
+    #     imageData['bbox_angle'][4]))
+    # box = np.int0(box)
+    # cv2.drawContours(image, [box], 0, (0, 191, 255), 2)
+    image = cv2.putText(image, className, org, font,
+                        fontScale, color, thickness, cv2.LINE_AA)
+    cv2.imshow("image", image)
+    cv2.waitKey()
+
+        ## BEGIN - draw rotated rectangle
+        # rect = cv2.minAreaRect(c)
+        # box = cv2.boxPoints(rect)
+        # box = np.int0(box)
+        # cv2.drawContours(image, [box], 0, (0, 191, 255), 2)
+        ## END - draw rotated rectangle
+
+    # cv2.imwrite('out.png', image)
+
+
+
+
+
 
 def visualize_bbox(img, bbox, class_name, color=BOX_COLOR, thickness=2):
     """Visualizes a single bounding box on the image"""
@@ -160,132 +194,103 @@ def dataGenerator(imagePath):
 		if total == 4:
 			break
 
-Classes = dataset_mapper.getClassinfo(TrainDir)
-TrainData = dataset_mapper.get_Ship_dicts(TrainDir)
-perClassData = dataset_mapper.ClassesTotals
-classDict = dataset_mapper.ClassesDicts
-classesNames = dataset_mapper.ClassesNames
-imagePerClass = dataset_mapper.imagePathPerClass
-print(perClassData)
+# Classes = dataset_mapper.getClassinfo(TrainDir)
+# TrainData = dataset_mapper.get_Ship_dicts(TrainDir)
+# perClassData = dataset_mapper.ClassesTotals
+# classDict = dataset_mapper.ClassesDicts
+# classesNames = dataset_mapper.ClassesNames
+# imagePerClass = dataset_mapper.imagePathPerClass
+# print(perClassData)
 
-for i in imagePerClass:
-	for k in range(0, len(imagePerClass[i])):
-		name= '/media/sujoy/New Volume/HRSC2016/Train/AllImages/100000637.bmp'
-
-		# bboxes,category_ids,max_w,max_h = getBoundingBox(imagePerClass[i][k])
-		bboxes, category_ids, max_w, max_h = getBoundingBox(name)
-
-		# transform = A.Compose([
-		# 	A.RandomSizedBBoxSafeCrop(width=700, height=700, erosion_rate=0.2,
-		# 	A.HorizontalFlip(p=0.7),
-		# 	A.RandomBrightnessContrast(brightness_limit=1, contrast_limit=1, p=0.8),
-		# ])
-		transform = A.Compose( [
-				A.RandomSizedBBoxSafeCrop(width=int(max_w),height=int(max_h),erosion_rate=0.2),
-				A.RandomScale(p=0.8),
-				A.Flip(p=0.7),
-				A.RandomBrightnessContrast(brightness_limit=.7, contrast_limit=.7, p=0.8)
-			],
-
-			bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']),
-		)
-		# counter=7
-		# while(counter < 50):
-
-		image = cv2.imread(name)
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		transformed = transform(image=image, bboxes=bboxes, category_ids=category_ids)
-
-		# another_transformed_image = transform(image=image, bboxes=bboxes, category_ids=category_ids)
-		# another_transformed_image1 = transform(image=image, bboxes=bboxes, category_ids=category_ids)
-		#
-		# visualize(
-		# 	transformed['image'],
-		# 	transformed['bboxes'],
-		# 	transformed['category_ids'],
-		# 	dataset_mapper.ClassesNames
-		# )
-		fileName = AugmentedDir + str.split((str.split(name,'/')[-1]),'.')[0] +'_'+str(k) +'.bmp'
-		cv2.imwrite(fileName, transformed['image'])
-		aug_image = {}
-		aug_image['file_name'] = fileName
-		aug_image['height'] = transformed['image'].shape[0]
-		aug_image['width'] = transformed['image'].shape[1]
-		objs=[]
-
-		for cat in range(0,len(transformed['category_ids'])):
-			obj = {}
-			obj['x_min'] = transformed['bboxes'][cat][0]
-			obj['y_min'] =transformed['bboxes'][cat][1]
-			obj['x_max'] =transformed['bboxes'][cat][2]
-			obj['y_max'] =transformed['bboxes'][cat][3]
-			obj['classID'] =transformed['category_ids'][cat]
-			objs.append(obj)
-		aug_image['annotation']=objs
-		# print(aug_image)
-		with open('aug_image_data.json', 'a') as json_file:
-			json.dump(aug_image, json_file,indent=2)
-
-
-		# visualize(
-		# 	another_transformed_image['image'],
-		# 	another_transformed_image['bboxes'],
-		# 	another_transformed_image['category_ids'],
-		# 	dataset_mapper.ClassesNames
-		# )
-		# visualize(
-		# 	another_transformed_image1['image'],
-		# 	another_transformed_image1['bboxes'],
-		# 	another_transformed_image1['category_ids'],
-		# 	dataset_mapper.ClassesNames
-		# )
-		# 	# print(counter)
-		# 	# counter +=7
-
-		break
-	break
-
-
-
-# construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True,
-# 	help="path to the input image")
-# ap.add_argument("-o", "--output", required=True,
-# 	help="path to output directory to store augmentation examples")
-# ap.add_argument("-t", "--total", type=int, default=100,
-# 	help="# of training samples to generate")
-# args = vars(ap.parse_args())
-
-# load the input image, convert it to a NumPy array, and then
-# reshape it to have an extra dimension
-# print("[INFO] loading example image...")
-# image = load_img(args["image"])
-# image = img_to_array(image)
-# image = np.expand_dims(image, axis=0)
-
-# construct the image generator for data augmentation then
-# initialize the total number of images generated thus far
-# aug = ImageDataGenerator(
-# 	rotation_range=30,
-# 	zoom_range=0.15,
-# 	width_shift_range=0.2,
-# 	height_shift_range=0.2,
-# 	shear_range=0.15,
-# 	horizontal_flip=True,
-# 	fill_mode="nearest")
-# total = 0
-
-# construct the actual Python generator
-# print("[INFO] generating images...")
-# imageGen = aug.flow(image, batch_size=1, save_to_dir=args["output"],
-# 	save_prefix="image", save_format="jpg")
+# for i in imagePerClass:
+# 	for k in range(0, len(imagePerClass[i])):
+# 		name= '/media/sujoy/New Volume/HRSC2016/Train/AllImages/100000637.bmp'
 #
-# # loop over examples from our image data augmentation generator
-# for image in imageGen:
-# 	# increment our counter
-# 	total += 1
+# 		# bboxes,category_ids,max_w,max_h = getBoundingBox(imagePerClass[i][k])
+# 		bboxes, category_ids, max_w, max_h = getBoundingBox(name)
 #
-# 	# if we have reached the specified number of examples, break
-# 	# from the loop
-# 	if total == args[
+# 		# transform = A.Compose([
+# 		# 	A.RandomSizedBBoxSafeCrop(width=700, height=700, erosion_rate=0.2,
+# 		# 	A.HorizontalFlip(p=0.7),
+# 		# 	A.RandomBrightnessContrast(brightness_limit=1, contrast_limit=1, p=0.8),
+# 		# ])
+# 		transform = A.Compose( [
+# 				A.RandomSizedBBoxSafeCrop(width=int(max_w),height=int(max_h),erosion_rate=0.2),
+# 				A.RandomScale(p=0.8),
+# 				A.Flip(p=0.7),
+# 				A.RandomBrightnessContrast(brightness_limit=.7, contrast_limit=.7, p=0.8)
+# 			],
+#
+# 			bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_ids']),
+# 		)
+# 		# counter=7
+# 		# while(counter < 50):
+#
+# 		image = cv2.imread(name)
+# 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# 		transformed = transform(image=image, bboxes=bboxes, category_ids=category_ids)
+#
+# 		# another_transformed_image = transform(image=image, bboxes=bboxes, category_ids=category_ids)
+# 		# another_transformed_image1 = transform(image=image, bboxes=bboxes, category_ids=category_ids)
+# 		#
+# 		# visualize(
+# 		# 	transformed['image'],
+# 		# 	transformed['bboxes'],
+# 		# 	transformed['category_ids'],
+# 		# 	dataset_mapper.ClassesNames
+# 		# )
+# 		fileName = AugmentedDir + str.split((str.split(name,'/')[-1]),'.')[0] +'_'+str(k) +'.bmp'
+# 		# cv2.imwrite(fileName, transformed['image'])
+# 		aug_image = {}
+# 		aug_image['file_name'] = fileName
+# 		aug_image['height'] = transformed['image'].shape[0]
+# 		aug_image['width'] = transformed['image'].shape[1]
+# 		objs=[]
+#
+# 		for cat in range(0,len(transformed['category_ids'])):
+# 			obj = {}
+# 			obj['x_min'] = transformed['bboxes'][cat][0]
+# 			obj['y_min'] =transformed['bboxes'][cat][1]
+# 			obj['x_max'] =transformed['bboxes'][cat][2]
+# 			obj['y_max'] =transformed['bboxes'][cat][3]
+# 			obj['classID'] =transformed['category_ids'][cat]
+# 			objs.append(obj)
+# 			# drawImage(fileName, transformed['bboxes'][cat], transformed['category_ids'][cat])
+# 		aug_image['annotation']=objs
+# 		# print(aug_image)
+#
+# 		with open('aug_image_data.json', 'a') as json_file:
+# 			json.dump(aug_image, json_file,indent=2)
+#
+#
+# 		# visualize(
+# 		# 	another_transformed_image['image'],
+# 		# 	another_transformed_image['bboxes'],
+# 		# 	another_transformed_image['category_ids'],
+# 		# 	dataset_mapper.ClassesNames
+# 		# )
+# 		# visualize(
+# 		# 	another_transformed_image1['image'],
+# 		# 	another_transformed_image1['bboxes'],
+# 		# 	another_transformed_image1['category_ids'],
+# 		# 	dataset_mapper.ClassesNames
+# 		# )
+# 		# 	# print(counter)
+# 		# 	# counter +=7
+#
+# 		break
+# 	break
+with open('aug_image_data.json') as f:
+	data = json.load(f)
+
+imagePath= data[0]['file_name']
+bboxes=[]
+cats = []
+for i in data[0]['annotation']:
+	bbox=[i['x_min'],i['y_min'],i['x_max'],i['y_max']]
+	cat = i['classID']
+	bboxes.append(bbox)
+	cats.append(cat)
+
+for i in range(0,4):
+	drawImage(imagePath,bboxes[i],cats[i])
